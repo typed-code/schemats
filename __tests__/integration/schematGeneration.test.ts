4af9cbf78a2ab7ce8a38da7795542fbb88d8a8e1
Felix Mosheev <9304194+felixmosh@users.noreply.github.com>
2019-08-10 21:41:35 +0300

    Change tests path

--- __tests__/integration/schematGeneration.test.ts
+++ __tests__/integration/schematGeneration.test.ts
@@ -7,22 +7,22 @@ describe('schemat generation integration testing', () => {
 
     beforeAll(async () => {
       db = getDatabase(process.env.POSTGRES_URL as string);
-      await loadSchema(db, './test/fixture/postgres/initCleanup.sql');
+      await loadSchema(db, './__tests__/fixture/postgres/initCleanup.sql');
     });
 
     it('Basic generation', async () => {
-      const inputSQLFile = 'test/fixture/postgres/osm.sql';
-      const outputFile = './test/actual/postgres/osm.ts';
-      const config = './test/fixture/postgres/osm.json';
+      const inputSQLFile = '__tests__/fixture/postgres/osm.sql';
+      const outputFile = './__tests__/actual/postgres/osm.ts';
+      const config = './__tests__/fixture/postgres/osm.json';
       await writeTsFile(inputSQLFile, config, outputFile, db);
 
       expect(outputFile).toMatchSnapshot();
     });
 
     it('Camelcase generation', async () => {
-      const inputSQLFile = 'test/fixture/postgres/osm.sql';
-      const outputFile = './test/actual/postgres/osm-camelcase.ts';
-      const config = './test/fixture/postgres/osm-camelcase.json';
+      const inputSQLFile = '__tests__/fixture/postgres/osm.sql';
+      const outputFile = './__tests__/actual/postgres/osm-camelcase.ts';
+      const config = './__tests__/fixture/postgres/osm-camelcase.json';
       await writeTsFile(inputSQLFile, config, outputFile, db);
 
       expect(outputFile).toMatchSnapshot();
@@ -34,22 +34,22 @@ describe('schemat generation integration testing', () => {
 
     beforeAll(async () => {
       db = getDatabase(`${process.env.MYSQL_URL}?multipleStatements=true`);
-      await loadSchema(db, './test/fixture/mysql/initCleanup.sql');
+      await loadSchema(db, './__tests__/fixture/mysql/initCleanup.sql');
     });
 
     it('Basic generation', async () => {
-      const inputSQLFile = 'test/fixture/mysql/osm.sql';
-      const outputFile = './test/actual/mysql/osm.ts';
-      const config = './test/fixture/mysql/osm.json';
+      const inputSQLFile = '__tests__/fixture/mysql/osm.sql';
+      const outputFile = './__tests__/actual/mysql/osm.ts';
+      const config = './__tests__/fixture/mysql/osm.json';
       await writeTsFile(inputSQLFile, config, outputFile, db);
 
       expect(outputFile).toMatchSnapshot();
     });
 
     it('Enum conflict in columns', async () => {
-      const inputSQLFile = 'test/fixture/mysql/conflict.sql';
-      const outputFile = './test/actual/mysql/conflict.ts';
-      const config = './test/fixture/mysql/conflict.json';
+      const inputSQLFile = '__tests__/fixture/mysql/conflict.sql';
+      const outputFile = './__tests__/actual/mysql/conflict.ts';
+      const config = './__tests__/fixture/mysql/conflict.json';
       try {
         await writeTsFile(inputSQLFile, config, outputFile, db);
       } catch (e) {
